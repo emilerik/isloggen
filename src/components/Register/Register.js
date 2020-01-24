52e8749348ade8a1b1f229751e3cdf6cf04d23c7
emilerik <emil.eriksson69@gmail.com>
2020-01-24 22:48:28 +0100

    Updated gitignore to include /api. Moved Register functions to its own component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,120 +1,100 @@
 import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import {Form, Button} from "semantic-ui-react";
+import { Auth0Lock } from "auth0-lock";
+
+var options = {
+    initialScreen: 'signUp',
+    allowLogin: false,
+    theme: {
+        logo: 'https://projecthydro.org/wp-content/uploads/2019/01/snowflake-300x300.png',
+        primaryColor: '#1394FF'
+    },
+    languageDictionary: {
+        emailInputPlaceholder: "exempel@email.se",
+        title: "Isinfo",
+        loginLabel: 'Logga in',
+        loginSubmitLabel: 'Logga in',
+        databaseAlternativeSignUpInstructions: 'eller',
+        signUpWithLabel: 'Registrera dig med %s',
+        signUpTitle: 'Isinfo',
+        signUpLabel: 'Registrera dig',
+        signUpTerms: '',
+        signUpSubmitLabel: 'Registrera nu',
+        usernameInputPlaceholder: 'ditt användarnamn',
+        passwordInputPlaceholder: 'ditt lösenord',
+        usernameOrEmailInputPlaceholder: 'användarnamn/email'
+    },
+};
+
+var lock = new Auth0Lock('XEx3hV9bQPknS835BgW07rZ6qdCaPzEb', 'isinfo.eu.auth0.com', options, {
+
+    auth: {
+        redirectUrl: 'http://localhost:3000',
+        responseType: 'code',
+        params: {
+            scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
+        }
+    }
+});
 
 class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      name: "",
-      password: "",
-      missingField: false,
-      incorrectSubmission: false
-    };
-  }
-
-  onSubmitRegister = () => {
-    //console.log(this.state);
-    const { name, email, password } = this.state;
-    //console.log(Boolean(name && email && password));
-    if (name && email && password) {
-      this.setState({ missingField: false });
-      fetch("http://localhost:3000/register", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          password: password
-        })
-      })
-        .then(response =>
-          response.ok
-            ? response.json()
-            : this.setState({ incorrectSubmission: true })
-        )
-        .then(user => {
-          //console.log(user);
-          this.setState({ incorrectSubmission: false });
-        })
-        .catch(err => {
-          console.log(err);
-          console.log("error raised");
-          this.setState({ missingField: true });
-        });
-    } else {
-      this.setState({ missingField: true });
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            name: "",
+            password: "",
+            missingField: false,
+            incorrectSubmission: false
+        };
     }
-    //console.log(this.state);
-  };
 
-  onEmailChange = event => {
-    this.setState({
-      email: event.target.value
-    });
-  };
-  onNameChange = event => {
-    this.setState({
-      name: event.target.value
-    });
-  };
-  onPasswordChange = event => {
-    this.setState({
-      password: event.target.value
-    });
-  };
+    onSubmitRegister = () => {
+        //console.log(this.state);
+        const {name, email, password} = this.state;
+        //console.log(Boolean(name && email && password));
+        if (name && email && password) {
+            this.setState({missingField: false});
+            fetch("http://localhost:3001/register", {
+                method: "post",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            })
+                .then(response =>
+                    response.ok
+                        ? response.json()
+                        : this.setState({incorrectSubmission: true})
+                )
+                .then(user => {
+                    //console.log(user);
+                    this.setState({incorrectSubmission: false});
+                })
+                .catch(err => {
+                    console.log(err);
+                    console.log("error raised");
+                    this.setState({missingField: true});
+                });
+        } else {
+            this.setState({missingField: true});
+        }
+        //console.log(this.state);
+    };
 
-  render() {
-    return (
-      <div className="dropdown ph3">
-        <p
-          className="f4 pointer white ma0"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Registrera
-        </p>
-        <div
-          className="dropdown-menu dropdown-menu-right ma3"
-          aria-labelledby="dropdownMenuButton"
-        >
-          <Form className="pa3 ma2 mt0" direction="left">
-            <Form.Input
-              type="name"
-              name="name"
-              id="name"
-              label="Användarnamn"
-              onChange={this.onNameChange}
-            />
-            <Form.Input
-              type="email"
-              name="email-address"
-              id="email-address"
-              label="Email"
-              onChange={this.onEmailChange}
-            />
-            <Form.Input
-              label="Lösenord"
-              type="password"
-              name="password"
-              id="password"
-              onChange={this.onPasswordChange}
-            />
-            <Button onClick={this.onSubmitRegister} primary>
-              Registera nu
-            </Button>
-            {this.state.missingField ? (
-              <p className="red tc">Fyll i alla fält</p>
-            ) : this.state.incorrectSubmission ? (
-              <p className="red tc">Det gick inte att registrera</p>
-            ) : null}
-          </Form>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="dropdown ph3">
+                <p
+                    className="ph3 f4 pointer ma0"
+                    onClick={() => lock.show()}>
+                    Registrera dig
+                </p>
+            </div>
+        );
+    }
 }
 
 export default Register;
