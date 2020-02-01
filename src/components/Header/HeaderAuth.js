72ab3baeae9ea4b00ee98600a2f07b172e5c1ea5
emilerik <emil.eriksson69@gmail.com>
2020-02-01 21:43:08 +0100

    Major change in Header. - Switched around the order of the menu items - Reduced usage of tachyons, moved to css instead - Added a media query in CSS to make it responsive for smaller screens - Added some logic such as isMenyVisible and isSmallScreen to help with responsiveness

--- src/components/Header/HeaderAuth.js
+++ src/components/Header/HeaderAuth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SignIn from "../SignIn/SignIn";
 import Register from "../Register/Register";
 import "./HeaderAuth.css";
@@ -6,52 +6,74 @@ import NewPost from "../NewPost/NewPost";
 import Nav from "react-bootstrap/Nav";
 import {useAuth0} from "../../react-auth0-wrapper";
 import {Link} from "react-router-dom";
+import MenuSymbol from "../../assets/menu-icon.png";
+
 
 const Header = () => {
-    const {isAuthenticated, loginWithRedirect, loginWithPopup, logout, loading, user} = useAuth0();
+    const {loading, isAuthenticated, loginWithRedirect, loginWithPopup, logout, user} = useAuth0();
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    if (loading) {
-        return (<div>Loading...</div>);
-    }
+    const toggleIsMenuVisible = () => {
+        setIsMenuVisible(!isMenuVisible);
+    };
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 700px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        };
+    }, []);
+
+    const handleMediaQueryChange = mediaQuery => {
+        if (mediaQuery.matches) {
+            setIsSmallScreen(true);
+        } else {
+            setIsSmallScreen(false);
+        }
+    };
 
     return (
-        <header
-            className="w-100 white pa3 ph5 mh2 mt0 mb2 h-ns"
-            style={{backgroundColor: "rgba(0, 0, 0, 0.3)"}}
-        >
-            <div>
-                {(isAuthenticated && !loading) ? (
-                    <div className="w-third fr flex items-center justify-end ph3 ma0">
-                        <NewPost user_email={user.email}/>
-                        <p className="ph3 f4 pointer" onClick={() => logout()}>
-                            Logga ut
-                        </p>
-                    </div>
-                ) : (
-                    <div className="w-third fr flex justify-end pa3 ma0">
-                        <p
-                            className="ph3 f4 pointer ma0"
-                            onClick={() => loginWithRedirect({})}
-                        >
-                            Logga in
-                        </p>
-                        <Register/>
-                    </div>
-                )}
+        <header className="mh2 mt0">
+            <div className="nav">
+                <Link to="/" className="ph3 ma0 f2 white logo" onClick={() => setIsMenuVisible(false)}>
+                    ❄ Isinfo ❄
+                </Link>
+                <button onClick={toggleIsMenuVisible} className="menubutton">
+                    <img src={MenuSymbol} alt="menubutton"/>
+                </button>
             </div>
-            <Link to="/">
-                <div className="w-third fr f2 pa3 tc white ma0">❄ Isinfo ❄</div>
-            </Link>
-            {isAuthenticated && (
-                <div className="w-third fr flex items-center justify-start pa3 ma0">
-                    <Link to="/">
-                        <p className="ph3 f4 ma0 white">Hem</p>
-                    </Link>
-                    <Link to="/profile">
-                        <p className="ph3 f4 ma0 white">Min profil</p>
-                    </Link>
+            {(isMenuVisible || !isSmallScreen) && (
+                <div className="menu">
+                    {/*Show "Ny rapport" and logout buttons if user is logged in*/}
+                    {(isAuthenticated && !loading) ? (
+                        <div className="fr items-center ma0">
+                            <Link to="/profile" className="ph3 f4 ma0 white">
+                                Min profil
+                            </Link>
+                            <NewPost user_email={user.email}/>
+                            <a className="ph3 f4 pointer" onClick={() => logout()}>
+                                Logga ut
+                            </a>
+                        </div>
+                    ) : (
+                        <div className="flex justify-end pa3 ma0">
+                            {/* Show Login and Register buttons if user signed out*/}
+                            <p
+                                className="ph3 f4 pointer ma0"
+                                onClick={() => loginWithRedirect({})}
+                            >
+                                Logga in
+                            </p>
+                            <Register/>
+                        </div>
+                    )}
                 </div>
             )}
+            {/* Show Home and Profile button if user logged in*/}
         </header>
     );
 };
